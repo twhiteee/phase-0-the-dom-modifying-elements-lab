12dea265bb22bfe2ae36c91e6363517a61b17259
Tristan Whitehouse <tsw54@cornell.edu>
2022-12-19 16:40:06 -0500

    commit tree 668eebc2f1c9c7942cd2424b5458c0fbdfa606a3

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+// Write your code here
+document.getElementById("main").remove();
+const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+newHeader.id = "victory"
+newHeader.textContent = "Tristan is the champion"
